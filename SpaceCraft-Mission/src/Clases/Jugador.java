4d0d8437d85d4e17255a7f5c1b1b9e5508291073
Markel <Markel@192.168.1.6>
2016-10-26 21:46:56 +0200

    Añadida la clase Jugador.

---
+++ SpaceCraft-Mission/src/Clases/Jugador.java
@@ -0,0 +1,39 @@
+package Clases;
+
+public class Jugador {
+	
+	private String nombre;
+	private Configuracion confi;
+	public Jugador(String nombre, Configuracion confi) {
+		this.nombre = nombre;
+		this.confi = confi;
+	}
+	
+	public Jugador(){
+		
+		this.nombre = " ";
+		this.confi = new Configuracion();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Configuracion getConfi() {
+		return confi;
+	}
+
+	public void setConfi(Configuracion confi) {
+		this.confi = confi;
+	}
+	
+	
+	
+	
+	
+
+}
