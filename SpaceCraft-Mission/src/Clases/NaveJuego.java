d62c7194840e354da504bda5d9644e63cef2d8c9
Markel <markel.alvarez@opendeusto.es>
2016-10-27 15:57:25 +0200

    asdasd

--- SpaceCraft-Mission/src/Clases/NaveJuego.java
+++ SpaceCraft-Mission/src/Clases/NaveJuego.java
@@ -1,5 +1,8 @@
 package Clases;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class NaveJuego extends Nave {
 private JLabelNave miGrafico;  // Etiqueta gráfica del coche
+private int hola;
 }
