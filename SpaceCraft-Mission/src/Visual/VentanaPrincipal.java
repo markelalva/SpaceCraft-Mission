bc7a912296c0678e28901c1544a74d24744a831b
Markel <markel.alvarez@opendeusto.es>
2017-01-03 20:26:41 +0100

    Añadido el uso de ficheros de texto.

--- SpaceCraft-Mission/src/Visual/VentanaPrincipal.java
+++ SpaceCraft-Mission/src/Visual/VentanaPrincipal.java
@@ -7,12 +7,21 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import Logica.Configuracion;
+import Logica.Log;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.logging.Level;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -24,6 +33,7 @@ public class VentanaPrincipal extends JFrame {
 	private JButton ElegirMundo;
 	private JButton MenuConfiguracion;
 	private JLabel NombreUsuario;
+	private JButton btnExportarDatos;
 
 	/**
 	 * Launch the application.
@@ -43,39 +53,73 @@ public class VentanaPrincipal extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-
-		ElegirMundo = new JButton("Seleccionar Nivel");
-		ElegirMundo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ElegirMundo em = new ElegirMundo(usuario);
-				em.setVisible(true);
-				dispose();
-			}
-		});
-
-		ElegirMundo.setBounds(180, 118, 133, 28);
-		contentPane.add(ElegirMundo);
-
-		MenuConfiguracion = new JButton("Configuraciï¿½n");
-		MenuConfiguracion.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-				VentanaConfiguracion vc = new VentanaConfiguracion(usuario);
-				vc.setVisible(true);
-				dispose();
-			}
-
-		});
-		MenuConfiguracion.setBounds(180, 185, 133, 28);
-		contentPane.add(MenuConfiguracion);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.UNRELATED_GAP_COLSPEC,
+				ColumnSpec.decode("46px"),
+				ColumnSpec.decode("124px"),
+				ColumnSpec.decode("165px"),},
+			new RowSpec[] {
+				RowSpec.decode("23px"),
+				RowSpec.decode("46px"),
+				RowSpec.decode("49px"),
+				RowSpec.decode("28px"),
+				RowSpec.decode("39px"),
+				RowSpec.decode("28px"),
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
 
 		JLabel lblNewLabel = new JLabel("Menu Principal (Cambiar por un icono)");
-		lblNewLabel.setBounds(180, 23, 165, 46);
-		contentPane.add(lblNewLabel);
+		contentPane.add(lblNewLabel, "4, 2, fill, fill");
 
 		NombreUsuario = new JLabel(usuario.getNombre());
-		NombreUsuario.setBounds(10, 23, 46, 14);
-		contentPane.add(NombreUsuario);
+		contentPane.add(NombreUsuario, "2, 2, fill, top");
+		
+				MenuConfiguracion = new JButton("Configuraciï¿½n");
+				MenuConfiguracion.addActionListener(new ActionListener() {
+
+					public void actionPerformed(ActionEvent e) {
+						VentanaConfiguracion vc = new VentanaConfiguracion(usuario);
+						vc.setVisible(true);
+						dispose();
+					}
+
+				});
+				
+						ElegirMundo = new JButton("Seleccionar Nivel");
+						ElegirMundo.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								ElegirMundo em = new ElegirMundo(usuario);
+								em.setVisible(true);
+								dispose();
+							}
+						});
+						contentPane.add(ElegirMundo, "4, 5, left, top");
+				contentPane.add(MenuConfiguracion, "4, 10, left, fill");
+		
+		btnExportarDatos = new JButton("Exportar Datos");
+		btnExportarDatos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Log.Loggear("Sus datos serán exportados a un .txt " , Level.INFO);
+				PrintStream fich = null;
+				try {
+					fich = new PrintStream(new FileOutputStream("Puntuaciones_" + usuario.getNombre()));
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				fich.println("Nombre: " + usuario.getNombre() );
+				fich.println("Color De Nave Seleccionado : " + usuario.getConfi().getColorNave() );
+				fich.println("Teclas a usar: " + usuario.getConfi().getTeclas() );
+				fich.println("Máxima puntuación obtenida en el mundo 1: " + usuario.getMaxPunt1());
+				fich.println("Máxima puntuación obtenida en el mundo 2: " + usuario.getMaxPunt2());
+				fich.println("Máxima puntuación obtenida en el mundo 3: " + usuario.getMaxPunt3());
+				
+			}
+		});
+		contentPane.add(btnExportarDatos, "2, 12, 2, 1");
 	}
 }
